09058993067da1f3307017fad148570addcee516
Pralin Dhungana <pralin.dhungana@gmail.com>
2026-04-10 17:31:25 +0930

    Added RepairRequestListView and basic list page setup

--- proj_1/housing/views.py
+++ proj_1/housing/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import RepairRequest
 
-# Create your views here.
+
+class RepairRequestListView(ListView):
+    model = RepairRequest
+    template_name = "housing/repair_request_list.html"
+    context_object_name = "requests"
