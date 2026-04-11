f791c940e8eaa7df6a36e86ca32c289da4010b3a
Pralin Dhungana <pralin.dhungana@gmail.com>
2026-04-11 17:42:00 +0930

    Added repair request create page using ModelForm and CreateView

--- proj_1/housing/urls.py
+++ proj_1/housing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RepairRequestListView, RepairRequestDetailView
+from .views import RepairRequestListView, RepairRequestDetailView, RepairRequestCreateView
 
 urlpatterns = [
     path("requests/", RepairRequestListView.as_view(), name="repair-request-list"),
     path("requests/<int:pk>/", RepairRequestDetailView.as_view(), name="repair-request-detail"),
+    path("requests/new/", RepairRequestCreateView.as_view(), name="repair-request-create"),
 ]
--- proj_1/housing/views.py
+++ proj_1/housing/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from .models import RepairRequest
+from .forms import RepairRequestForm
 
 
 class RepairRequestListView(ListView):
@@ -12,3 +14,10 @@ class RepairRequestDetailView(DetailView):
     model = RepairRequest
     template_name = "housing/repair_request_detail.html"
     context_object_name = "request"
+
+
+class RepairRequestCreateView(CreateView):
+    model = RepairRequest
+    form_class = RepairRequestForm
+    template_name = "housing/repair_request_form.html"
+    success_url = reverse_lazy("repair-request-list")
