7751e7c3f51c2353d3f2daa95379647dc112b8f0
Masafy Kamran <umairsabirjutt@gmail.com>
2026-04-15 17:52:23 +0500

    history page

--- proj_1/housing/urls.py
+++ proj_1/housing/urls.py
@@ -4,11 +4,16 @@ from .views import (
     RepairRequestDetailView,
     RepairRequestCreateView,
     RepairRequestUpdateView,
+    MaintenanceHistoryView,
+    HomeView
 )
 
 urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
     path("requests/", RepairRequestListView.as_view(), name="repair-request-list"),
     path("requests/new/", RepairRequestCreateView.as_view(), name="repair-request-create"),
     path("requests/<int:pk>/", RepairRequestDetailView.as_view(), name="repair-request-detail"),
     path("requests/<int:pk>/edit/", RepairRequestUpdateView.as_view(), name="repair-request-update"),
+    # --- Habiba: Maintenance History URL ---
+    path("history/", MaintenanceHistoryView.as_view(), name="maintenance-history"),
 ]
--- proj_1/housing/views.py
+++ proj_1/housing/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView,TemplateView
 from django.urls import reverse_lazy
 from .models import RepairRequest
 from .forms import RepairRequestForm
-
-
+# --- Habiba: Home View ---
+class HomeView(TemplateView):
+    template_name = "housing/home.html"
 class RepairRequestListView(ListView):
     model = RepairRequest
     template_name = "housing/repair_request_list.html"
@@ -28,3 +29,12 @@ class RepairRequestUpdateView(UpdateView):
     form_class = RepairRequestForm
     template_name = "housing/repair_request_form.html"
     success_url = reverse_lazy("repair-request-list")
+
+# --- Habiba: Maintenance History View ---
+class MaintenanceHistoryView(ListView):
+    model = RepairRequest
+    template_name = "housing/maintenance_history.html"
+    context_object_name = "completed_requests"
+ 
+    def get_queryset(self):
+        return RepairRequest.objects.filter(status="completed").order_by("-updated_at")
