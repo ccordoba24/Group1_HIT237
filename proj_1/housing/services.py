812d935bc06b217866da26b6dde9117897c0b575
Pralin Dhungana <pralin.dhungana@gmail.com>
2026-05-27 10:45:31 +0930

    Allowed tenant linked users to update request details

--- proj_1/housing/services.py
+++ proj_1/housing/services.py
@@ -7,17 +7,29 @@ from .models import RepairRequest
 class PermissionService:
     @staticmethod
     def is_staff_or_superuser(user):
-        return user.is_authenticated and (user.is_staff or user.is_superuser)
+        return (
+            user.is_authenticated
+            and (
+                user.is_staff
+                or user.is_superuser
+            )
+        )
 
     @staticmethod
     def can_update_repair_request(user, repair_request):
         if not user.is_authenticated:
             return False
 
-        if user.is_staff or user.is_superuser:
+        if PermissionService.is_staff_or_superuser(user):
             return True
 
-        return repair_request.created_by == user
+        if repair_request.created_by == user:
+            return True
+
+        if repair_request.tenant and repair_request.tenant.user == user:
+            return True
+
+        return False
 
     @staticmethod
     def can_add_maintenance_update(user):
